2be06096624fe19278eefe8df612655bc6653605
Hams <mhams849@gmail.com>
2026-02-07 07:29:57 -0800

    login changes

--- src/main/java/com/example/Reservation/security/AuthController.java
+++ src/main/java/com/example/Reservation/security/AuthController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/auth")
 public class AuthController {
 
